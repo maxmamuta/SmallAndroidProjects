cc94ee89b4e3bba54f28bb512372ddf0d1ce32f7
MolarMak <molarmak@gmail.com>
2017-02-25 09:29:16 +0200

    Add networking in PhotoGallery app

---
+++ PhotoGallery/app/src/main/java/com/maxmamuta/photogallery/FlickrFetchr.java
@@ -0,0 +1,43 @@
+package com.maxmamuta.photogallery;
+
+import android.view.inputmethod.InputConnection;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by Maxim on 2/24/2017.
+ */
+
+public class FlickrFetchr {
+    byte []getUrlBytes(String urlSpec) throws IOException {
+        URL url = new URL(urlSpec);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            InputStream in = connection.getInputStream();
+            if (connection.getResponseCode()  != HttpURLConnection.HTTP_OK)
+                return null;
+
+            int bytesRead = 0;
+            byte []buffer = new byte[1024];
+            while ((bytesRead = in.read(buffer)) > 0) {
+                out.write(buffer, 0, bytesRead);
+            }
+            out.close();
+
+            return out.toByteArray();
+        } finally {
+            connection.disconnect();
+        }
+    }
+
+    public String getUrl(String urlSpec) throws IOException {
+        return new String(getUrlBytes(urlSpec));
+    }
+}
--- PhotoGallery/app/src/main/java/com/maxmamuta/photogallery/PhotoGalleryFragment.java
+++ PhotoGallery/app/src/main/java/com/maxmamuta/photogallery/PhotoGalleryFragment.java
@@ -1,10 +1,55 @@
 package com.maxmamuta.photogallery;
 
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.widget.GridView;
+
+import java.io.IOException;
 
 /**
  * Created by Maxim on 2/24/2017.
  */
 
 public class PhotoGalleryFragment extends Fragment {
+    private GridView mGridView;
+    private View v;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setRetainInstance(true);
+
+        new FetchItemsTask().execute();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+
+        mGridView = (GridView) v.findViewById(R.id.gridView);
+
+        return v;
+    }
+
+    private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                String result = new FlickrFetchr().getUrl("http://www.google.com");
+                Log.i("RESSSSULT", "Fetched contents of URL: " + result);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
 }
